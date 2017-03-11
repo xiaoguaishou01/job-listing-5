5492f64e21b873d737d6ade9c62e4ca01aaf09e0
weiyueyan <xiaoguaishou0809@gmail.com>
2017-03-11 14:02:56 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -39,11 +39,11 @@ class Admin::JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
-  
+
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
