12d402c35a538503aa069789b37958616deb4b69
weiyueyan <xiaoguaishou0809@gmail.com>
2017-04-25 14:42:44 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+#u = User.new
+#u.email = "admin@test.com"           # 可以改成自己的 email
 
-u.password = "123456"                # 最少要六码
+#u.password = "123456"                # 最少要六码
 
-u.password_confirmation = "123456"   # 最少要六码
+#u.password_confirmation = "123456"   # 最少要六码
 
-u.is_admin = true
-u.save
+#u.is_admin = true
+#u.save
+
+
+ fenlei = '程序员,技术,产品,运营,设计,市场与营销,职能,金融,管理'.split(',')
+city = '北京,上海,广州,深圳,杭州'.split(',')
+name = '迅达科技有限公司,中智控股中心,IWOT,腾讯控股有限公司,友宝移动有限公司'.split(',')
+create_jobs = for x in 1..5 do
+    case x
+    when 1
+for i in 1..9 do
+  Job.create!([title: "Job no.#{i}", description: "这是用种子建立的第 #{i} 个Hidden工作",sort: fenlei[i-1] ,CompanyName: name[x-1] ,CompanyLocation: city[x-1] , wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "false"])
+end
+    when 2
+for i in 1..9 do
+  Job.create!([title: "Job no.#{i}", description: "这是用种子建立的第 #{i} 个Hidden工作",sort: fenlei[i-1] ,CompanyName: name[x-1] ,CompanyLocation: city[x-1] , wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "false"])
+end
+
+    when 3
+for i in 1..9 do
+  Job.create!([title: "Job no.#{i}", description: "这是用种子建立的第 #{i} 个Hidden工作",sort: fenlei[i-1] ,CompanyName: name[x-1] ,CompanyLocation: city[x-1] , wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "false"])
+end
+
+    when 4
+for i in 1..9 do
+  Job.create!([title: "Job no.#{i}", description: "这是用种子建立的第 #{i} 个Hidden工作",sort: fenlei[i-1] ,CompanyName: name[x-1] ,CompanyLocation: city[x-1] , wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "false"])
+end
+
+    when 5
+for i in 1..9 do
+  Job.create!([title: "Job no.#{i}", description: "这是用种子建立的第 #{i} 个Hidden工作",sort: fenlei[i-1] ,CompanyName: name[x-1] ,CompanyLocation: city[x-1] , wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "false"])
+end
+    else
+  end
+end
